4d094b69b00ba7ff99426b193191feadd059b035
Martin Trigaux <mat@odoo.com>
2017-11-09 20:45:01 +0100

    Make view dynamic Convert tabs to space Missing import

--- kicker/controllers/kicker.py
+++ kicker/controllers/kicker.py
@@ -1,8 +1,9 @@
 from odoo import http
-
+from odoo.http import request
 
 class KickerController(http.Controller):
 
     @http.route(['/free'], type='http', auth="public")
     def is_the_kicker_free(self):
-        return request.render('kicker.page_is_free', {})
+        is_free = True
+        return request.render('kicker.page_is_free', {'is_free': is_free})
