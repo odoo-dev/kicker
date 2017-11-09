0a3b7532b0137bbea85cab3878f0ee897f80f211
jem-odoo <jem@openerp.com>
2017-11-09 19:13:19 +0100

    [ADD] kw_cam, kicker: add empty modules for kicker management

---
+++ hw_cam/__manifest__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+{
+    'name': 'Webcam Driver',
+    'version': '1.0',
+    'category': 'Hardware Drivers',
+    'sequence': 6,
+    'summary': 'Provides support for camera',
+    'website': 'https://kicker.odoo.com',
+    'depends': ['base'],
+}
---
+++ kicker/__manifest__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+{
+    'name': 'Kicker App',
+    'version': '1.0',
+    'category': 'Kicker',
+    'sequence': 6,
+    'summary': 'Kicker in the lunch Room',
+    'website': 'https://kicker.odoo.com',
+    'depends': ['base', 'http_routing'],
+}
